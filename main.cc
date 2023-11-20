bc907450a65ac069df8d817d6be9b9e2a99def14
Adeline Su <a26su@uwaterloo.ca>
2023-11-19 20:27:18 -0500

    added more to main

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include "gamemaster.h"
 using namespace std;
 
+// helper function to find command line variabls
 bool findIndex(int argc, char* argv[], string s, int* i) {
     for (int index = 0; index < argc; ++i) {
         if (argv[index] == s) *i = index; return true;
@@ -16,22 +17,24 @@ bool findIndex(int argc, char* argv[], string s, int* i) {
 int main(int argc, char *argv[]) {
     // potential command line arguments: -deck1 (filename), -deck2 (filename), -init (filename), -testing
     int* i;
-    bool deck1, deck2, init, testing;
+    bool deck1Flag, deck2Flag, initFlag, testingFlag;
     string deck1File, deck2File, initFile;
-    if (findIndex(argc, argv, "-deck1", i)) deck1 = true; deck1File = argv[*i + 1];
-    if (findIndex(argc, argv, "-deck2", i)) deck2 = true; deck2File = argv[*i + 1];
-    if (findIndex(argc, argv, "-init", i)) init = true; initFile = argv[*i + 1];
-    if (findIndex(argc, argv, "-testing", i)) testing = true; 
+    if (findIndex(argc, argv, "-deck1", i)) deck1Flag = true; deck1File = argv[*i + 1];
+    if (findIndex(argc, argv, "-deck2", i)) deck2Flag = true; deck2File = argv[*i + 1];
+    if (findIndex(argc, argv, "-init", i)) initFlag = true; initFile = argv[*i + 1];
+    if (findIndex(argc, argv, "-testing", i)) testingFlag = true; 
 
     // set decks according to cmd line args, or default.deck
-    ifstream in1 = deck1 ? ifstream(deck1File.c_str()) : ifstream("default.deck");
-    ifstream in2 = deck2 ? ifstream(deck2File.c_str()) : ifstream("default.deck");
+    ifstream in1 = deck1Flag ? ifstream(deck1File.c_str()) : ifstream("default.deck");
+    ifstream in2 = deck2Flag ? ifstream(deck2File.c_str()) : ifstream("default.deck");
 
     string cardName;
     while (getline(in1, cardName)) {
-        // (create the card that corresponds to cardName)
+        // (construct the card that corresponds to cardName)
     }
-    // (construct a Deck)
+    // initialize 2 decks
+    Deck deck1; // = new Deck{...}
+    Deck deck2; // = new Deck{...}
 
 
     // asks players for their names
@@ -40,7 +43,16 @@ int main(int argc, char *argv[]) {
     getline(cin, p1name); 
     getline(cin, p2name); 
 
+    // initialize 2 players
+    // Player p1{ ... }
+    // Player p2{ ... }
+
     // initialize the game
     GameMaster gm{p1name, p2name, deck1, deck2}; 
+
+    bool turn = 0; // 0 for player 1's turn, 1 for player 2's turn
+    while (true) {
+
+    }
 
 }
