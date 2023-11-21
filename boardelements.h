b7d2dfa4aa8a302faa917efc4838877bf61d4377
Adeline Su <a26su@uwaterloo.ca>
2023-11-20 21:33:57 -0500

    removed 4 redundent board elements

--- boardelements.h
+++ boardelements.h
@@ -2,26 +2,33 @@
 
 #ifndef __BOARDELEMENTS_H__
 #define __BOARDELEMENTS_H__
+#include <stack>
 #include "card.h"
+#include "minion.h"
+using namespace std;
 
 // collection of cards from which players draw cards into their Hand
 class Deck {
+    // vector<Card*> theDeck;
+    vector<string*> theDeck;
 
+ public:
+    void shuffle();
+    Card* drawCard();
 };
-
 // cards a player is holding, but has not placed
 class Hand {
-
+    vector<Card*> theHand;
 };
 
 // Minions the player has played, not yet dead
 class Board {
-
+    vector<Minion*> theBoard;
 };
 
 // collection of dead Minions
 class Graveyard {
-
+    stack<Minion*> theGrave;
 };
 
 #endif
